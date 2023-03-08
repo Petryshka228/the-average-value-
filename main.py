b7edff2b40f77f1f094937d42f6ffd56048e04fa
Petryshka228 <123024502+Petryshka228@users.noreply.github.com>
2023-03-08 21:50:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
 print('Все права защищены')
-i = 1
-while i != 0:
-    b2 = 0
-    b3 = 0
-    b4 = 0
-    b5 = 0
-    er = 0
-    a = str(input('Перечислите оценки: ')).split()
+while True:
+    b1, b2, b3, b4, b5 = 0, 0, 0, 0, 0
+    a = (input('Перечислите числа через пробел: ')).split()
+    list1 = []
     for i in a:
-        if i == '2':
-            b2 += 1
-        elif i == '3':
-            b3 += 1
-        elif i == '4':
-            b4 += 1
-        elif i == '5':
-            b5 += 1
-        else:
-            er += 1
-    if er == 0:
-        kol = b2 + b3 + b4 + b5
-        b2 *= 2
-        b3 *= 3
-        b4 *= 4
-        b5 *= 5
-        sr = round(((b2 + b3 + b4 + b5) / kol), 2)
-        print('Средний балл равен: ', sr)
-        print('Для продолжения нажмите "Enter"')
-    else:
-        print('Ошибка, введите корректные данные')
-        print('Для продолжения нажмите "Enter"')
+        list1.append(int(i))
+    sr = round(sum(list1) / len(a), 2)
+    print('Средний балл равен: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
     input()
-
