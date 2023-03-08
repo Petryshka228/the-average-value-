28fe88b0478e5bb6597dfccc2aa841a753f7b69f
Petryshka228 <123024502+Petryshka228@users.noreply.github.com>
2023-03-08 22:28:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('Все права защищены')
 while True:
-    a = list((input('Перечислите оценки через пробел: ')).split())
+    a = list((input('Перечислите числа через пробел: ')).split())
     result = list(map(int, a))
     sr = round(sum(result) / len(a), 2)
-    print('Средний балл равен: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
+    print('Среднее значение равно: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
     input()
