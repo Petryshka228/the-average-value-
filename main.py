3e264d6a69cc4ef6edd37673c649e1c7c6862099
Petryshka228 <123024502+Petryshka228@users.noreply.github.com>
2023-03-20 19:03:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import numbers
+
 print('Все права защищены')
+
 while True:
-    a = list((input('Перечислите числа через пробел: ')).split())
-    result = list(map(int, a))
-    sr = round(sum(result) / len(a), 2)
-    print('Среднее значение равно: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
+    result = []
+    a = (input('Перечислите числа через пробел: ')).split()
+    for i in a:
+        try:
+            if isinstance(int(i), numbers.Number) == True:
+                result.append(i)
+            else:
+                print(1)
+        except:
+            er = i
+    if len(result) != 0:
+        itog = list(map(int, result))
+        sr = round(sum(itog) / len(result), 2)
+        print('Среднее значение равно: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
     input()
