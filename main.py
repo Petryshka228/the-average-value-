b2115497498c3e5b48780cfde306845b70d017a6
Petryshka228 <123024502+Petryshka228@users.noreply.github.com>
2023-03-20 20:35:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import numbers
 
+
 print('Все права защищены')
 
 while True:
     result = []
     a = (input('Перечислите числа через пробел: ')).split()
     for i in a:
-        try:
-            if isinstance(int(i), numbers.Number) == True:
-                result.append(i)
-            else:
-                print(1)
-        except:
-            er = i
+        if i.isnumeric() == True:
+            result.append(i)
     if len(result) != 0:
         itog = list(map(int, result))
         sr = round(sum(itog) / len(result), 2)
