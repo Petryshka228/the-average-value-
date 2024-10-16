b80fd7b0d907c8ccf6483ae9bf521b47adc318c2
Petryshka228 <123024502+Petryshka228@users.noreply.github.com>
2024-10-16 19:15:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 while True:
-    result = []
-    a = (input('Перечислите числа через пробел: ')).split()
-    for i in a:
-        if i.isnumeric() == True:
-            result.append(i)
-    if len(result) != 0:
-        itog = list(map(int, result))
-        sr = round(sum(itog) / len(result), 2)
-        print('Среднее значение равно: ', sr, '\n', '\n', 'Для продолжения нажмите "Enter"')
+
+    def get_average_value():
+        result = []
+        a = (input('Перечислите числа через пробел: ')).split()
+        for i in a:
+            if i.isnumeric():
+                result.append(i)
+        if len(result) != 0:
+            itog = list(map(int, result))
+        return round(sum(itog) / len(result), 2)
+
+
+    print('Среднее значение равно: ', get_average_value(), '\n', '\n', 'Для продолжения нажмите "Enter"')
     input()
